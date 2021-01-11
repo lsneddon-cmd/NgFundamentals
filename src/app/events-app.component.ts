5541eb8246001e0fc0003de00c243fc9438ce509
Lewis Sneddon <lewis.sneddon@icloud.com>
2021-01-11 08:29:16 +0000

    added Bootstrap and jQuery

--- src/app/events-app.component.ts
+++ src/app/events-app.component.ts
@@ -2,7 +2,10 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'event-app',
-  template: '<h1>Hello World!</h1>'
+  template: `
+    <h1>Hello World!</h1>
+    <img src="/assets/images/basic-shield.png">
+  `
 })
 export class EventsAppComponent {
   title = 'ng-Fundamentals';
