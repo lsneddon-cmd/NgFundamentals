3641c6394b0991c9e3d7a1df0d4c93aff8d53dc5
Lewis Sneddon <lewis.sneddon@icloud.com>
2021-01-11 16:58:50 +0000

    wrapping third party services - toastr

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,7 @@ import { EventsListComponent } from './events/events-list.component';
 import { EventThumbnailComponent } from './events/event-thumbnail/event-thumbnail.component';
 import { NavigationComponent } from './navigation/navigation.component';
 import { EventService } from './events/shared/event.service'
+import { ToastrService } from './common/toastr.service';
 
 @NgModule({
   imports: [
@@ -17,7 +18,7 @@ import { EventService } from './events/shared/event.service'
     EventThumbnailComponent,
     NavigationComponent
   ],
-  providers: [EventService],
+  providers: [EventService, ToastrService],
   bootstrap: [EventsAppComponent]
 })
 export class AppModule { }
